2384da0cb1eccd3b70d480b23784e05db7f5fdbe
Teige Gao <yiqgao@outlook.com>
2021-01-14 14:24:12 +0800

    remove sensitive information

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    publicPath: './',
+    outputDir: "dist", // 输出文件目录
+    lintOnSave: false, // eslint 是否在保存时检查
+    assetsDir: 'static', // 配置js、css静态资源二级目录的位置  
+    devServer:{
+      proxy:{
+        '/api/*': {
+          target: 'http://127.0.0.1:8081',
+          changeOrigin: true,
+          pathRewrite: {
+            '^/api':'/'
+          }
+        }
+      }
+    }
+  }
